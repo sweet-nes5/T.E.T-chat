2db56e81f613cbb3b2cdaca05a87c6becc67b7e6
sweet-nes5 <nesrine.bey5@gmail.com>
2025-02-08 00:10:47 +0100

    ignored index3 file

---
+++ db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS user(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    pwd TEXT NOT NULL,
+    created_at TEXT  NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS messages(
+    id INTEGER PRIMARY KEY,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    text TEXT,
+    created_at TEXT,
+);
+
+SELECT email, pwd FROM user;
+
+SELECT * FROM messages ORDER BY id DESC LIMIT 50;
+
